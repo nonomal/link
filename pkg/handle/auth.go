3cb83a012b1ac4a0e0dc8c46ef55600bda7b3cc1
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-11-06 10:22:09 +0800

    Create auth.go

---
+++ pkg/handle/auth.go
@@ -0,0 +1,41 @@
+package handle
+
+import (
+    "net"
+    "net/http"
+    "net/url"
+    "sync"
+
+    "github.com/yosebyte/link/pkg/autotls"
+)
+
+func Auth(parsedURL *url.URL, whiteList *sync.Map) error {
+    http.HandleFunc(parsedURL.Path, func(w http.ResponseWriter, r *http.Request) {
+        clientIP, _, err := net.SplitHostPort(r.RemoteAddr)
+        if err != nil {
+            return
+        }
+        if _, err := w.Write([]byte(clientIP + "\n")); err != nil {
+            return
+        }
+        whiteList.Store(clientIP, struct{}{})
+    })
+    if parsedURL.Scheme == "http" {
+        if err := http.ListenAndServe(parsedURL.Host, nil); err != nil {
+            return err
+        }
+    } else {
+        tlsConfig, err := autotls.Setup(parsedURL.User.Username(), parsedURL.Hostname())
+        if err != nil {
+            return err
+        }
+        authServer := &http.Server{
+            Addr:      parsedURL.Host,
+            TLSConfig: tlsConfig,
+        }
+        if err := authServer.ListenAndServeTLS("", ""); err != nil {
+            return err
+        }
+    }
+    return nil
+}
