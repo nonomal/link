492831dd88cb0ffbc70fadc153bccb862821d581
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-12-10 02:56:44 +0000

    Eliminate resource leakage issues

--- pkg/conn/conn.go
+++ pkg/conn/conn.go
@@ -9,14 +9,31 @@ func DataExchange(conn1, conn2 net.Conn) {
 	done := make(chan struct{}, 2)
 	go func() {
 		io.Copy(conn1, conn2)
+		sendEOF(conn1)
 		done <- struct{}{}
 	}()
 	go func() {
 		io.Copy(conn2, conn1)
+		sendEOF(conn2)
 		done <- struct{}{}
 	}()
 	<-done
 	<-done
-	conn1.Close()
-	conn2.Close()
+	closeConn(conn1)
+	closeConn(conn2)
+}
+
+func sendEOF(conn net.Conn) {
+	switch c := conn.(type) {
+	case *net.TCPConn:
+		c.CloseWrite()
+	default:
+		c.Close()
+	}
+}
+
+func closeConn(conn net.Conn) {
+	if conn != nil {
+		conn.Close()
+	}
 }
