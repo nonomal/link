349e631f485beb8bdd04772574d8352bc0f92b97
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-12-11 21:43:55 +0800

    Update conn.go

--- pkg/conn/conn.go
+++ pkg/conn/conn.go
@@ -8,32 +8,17 @@ import (
 func DataExchange(conn1, conn2 net.Conn) {
 	done := make(chan struct{}, 2)
 	go func() {
+		defer conn1.Close()
 		io.Copy(conn1, conn2)
-		sendEOF(conn1)
 		done <- struct{}{}
 	}()
 	go func() {
+		defer conn2.Close()
 		io.Copy(conn2, conn1)
-		sendEOF(conn2)
 		done <- struct{}{}
 	}()
 	<-done
 	<-done
 	closeConn(conn1)
 	closeConn(conn2)
-}
-
-func sendEOF(conn net.Conn) {
-	switch c := conn.(type) {
-	case *net.TCPConn:
-		c.CloseWrite()
-	default:
-		c.Close()
-	}
-}
-
-func closeConn(conn net.Conn) {
-	if conn != nil {
-		conn.Close()
-	}
 }
