54e0782fd0b7d8dc518834f881b4aa386afa9f36
𝐘𝐨𝐬𝐞𝐛𝐲𝐭𝐞 <54016243+yosebyte@users.noreply.github.com>
2024-12-13 03:48:29 +0000

    Add connection dynamic timeout settings

--- pkg/conn/conn.go
+++ pkg/conn/conn.go
@@ -4,12 +4,14 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 func DataExchange(conn1, conn2 net.Conn) error {
 	var (
 		once1, once2 sync.Once
 		wg           sync.WaitGroup
+		timeout      = 30 * time.Second
 	)
 	closeConn1 := func() {
 		once1.Do(func() {
@@ -25,6 +27,9 @@ func DataExchange(conn1, conn2 net.Conn) error {
 			}
 		})
 	}
+	updateDeadline := func(conn net.Conn) {
+		conn.SetDeadline(time.Now().Add(timeout))
+	}
 	errChan := make(chan error, 2)
 	wg.Add(2)
 	go func() {
@@ -33,22 +38,28 @@ func DataExchange(conn1, conn2 net.Conn) error {
 			closeConn1()
 			closeConn2()
 		}()
-		if _, err := io.Copy(conn1, conn2); err != nil {
-			closeConn1()
-			closeConn2()
-			errChan <- err
+		for {
+			updateDeadline(conn1)
+			updateDeadline(conn2)
+			if _, err := io.Copy(conn1, conn2); err != nil {
+				errChan <- err
+				return
+			}
 		}
 	}()
 	go func() {
 		defer func() {
 			wg.Done()
-			closeConn1()
 			closeConn2()
+			closeConn1()
 		}()
-		if _, err := io.Copy(conn2, conn1); err != nil {
-			closeConn1()
-			closeConn2()
-			errChan <- err
+		for {
+			updateDeadline(conn2)
+			updateDeadline(conn1)
+			if _, err := io.Copy(conn2, conn1); err != nil {
+				errChan <- err
+				return
+			}
 		}
 	}()
 	wg.Wait()
